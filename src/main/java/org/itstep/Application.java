a5e9e0ac5cec20bb4661db994bf448fb197eddec
Kiril <jolymod96@gmail.com>
2023-03-19 18:50:44 +0200

    Realized functionality

--- src/main/java/org/itstep/Application.java
+++ src/main/java/org/itstep/Application.java
@@ -1,7 +1,26 @@
 package org.itstep;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // Ваш код пишите здесь
+        Map<String, TreeSet<String>> networkMap = new HashMap<>();
+        var input = new Scanner(System.in);
+        for(var i = 0; i < 10; i++) {
+            System.out.print("Input tv network: ");
+            var network = input.nextLine();
+            System.out.print("Input tv show on " + network + ": ");
+            var show = input.nextLine();
+            var showsSet = networkMap.computeIfAbsent(network, k -> new TreeSet<>());
+            showsSet.add(show);
+            System.out.println(networkMap + "\n");
+        }
+        System.out.println("\nContents of networkMap:");
+        List<String> networks = new ArrayList<>(networkMap.keySet());
+        Collections.sort(networks);
+        for (var network : networks) {
+            System.out.println(network + ": " + networkMap.get(network));
+        }
+        // TreeSet автоматически сортирует свои элементы в порядке возрастания, поэтому не нужно сортировать ТВ-шоу в кажом канале вручную.
     }
 }
